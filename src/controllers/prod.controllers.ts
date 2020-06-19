8e835e7f321bbd745bf0b041605e81e02d8e1527
FabianCeciliano <fbnceciliano@gmail.com>
2020-06-18 21:13:33 -0600

    functions added (listLocations)

--- src/controllers/prod.controllers.ts
+++ src/controllers/prod.controllers.ts
@@ -3,7 +3,9 @@ import User, { IProduct } from '../models/product'
 import jwt, { JsonWebTokenError } from 'jsonwebtoken'
 import config from '../config/config'
 import Product from '../models/product'
+import myMap from '../Constants'
 
+const haversine = require('haversine')
 // Methods
 export const findAll = async ( req: Request , res: Response ): Promise<Response> => {
     console.log(req.body)
@@ -15,6 +17,23 @@ export const findAll = async ( req: Request , res: Response ): Promise<Response>
     })
 }
 
+export const getNearProducts = async ( req: Request , res: Response ): Promise<Response> => {
+    console.log(req.body)
+    if (!req.body.latitude || !req.body.longitude) {
+        return res.status(400).json({
+            msg: 'Please. Send coordenates'
+        })
+    }
+
+    const products = await Product.find({loc:getCloserLocation(req.body.latitude,req.body.longitude)})
+    console.log(products)
+    return res.status(200).send({
+        success: true,
+        data: products
+    })
+    
+}
+
 export const addProduct = async ( req: Request , res: Response ): Promise<Response> => {
     console.log(req.body)
     if (!req.body.name || !req.body.loc || !req.body.productor) {
@@ -23,34 +42,48 @@ export const addProduct = async ( req: Request , res: Response ): Promise<Respon
         })
     }
     // map zones
-    let locationNumber = 0
     const zoneName: string = req.body.loc
-    switch (zoneName.toLocaleLowerCase()) {
-        case 'cartago': {
-           locationNumber = 2
-           break
-        }
-        case 'el guarco': {
-            locationNumber = 8
-           break
-        }
-        case 'puriscal': {
-            locationNumber = 11
-            break
-        }
-        case 'paraiso': {
-            locationNumber = 5
-            break
-        }
-        default: {
-           return res.status(400).json({msg: 'Location not permitted'})
-        }
-     }
-    const product = await Product.findOne({name: req.body.name, loc: locationNumber, productor: req.body.productor})
+    
+    if(myMap.get(zoneName)==undefined){
+        return res.status(400).json({msg: 'Location not permitted'})
+    }
+
+    const product = await Product.findOne({name: req.body.name, loc: myMap.get(zoneName).zoneNumb, productor: req.body.productor})
     if ( product ) {
         return res.status(400).json({msg: 'The product at that location already exist', productor: req.body.productor})
     }
-    const newProduct = new Product({'name': req.body.name, 'productor': req.body.productor, 'loc': locationNumber })
+
+    const newProduct = new Product({'name': req.body.name, 'productor': req.body.productor, 'loc': myMap.get(zoneName).zoneNumb })
     await newProduct.save()
     return res.status(201).json(newProduct)
+}
+
+function getCloserLocation(platitude:any,plongitude:any):Number{
+    
+    let closer = Number.MAX_SAFE_INTEGER
+    let distance
+    var mapIter = myMap.keys()
+    let locationName:string
+    let zoneNumber:Number=0
+
+    const start ={
+        latitude:platitude,
+        longitude:plongitude
+    }
+
+    for (let i = 0; i < 4; i++) {
+        locationName=mapIter.next().value
+
+        distance =haversine(start,{latitude:myMap.get(locationName).lat,longitude:myMap.get(locationName).long},{unit: 'km'})
+        
+        console.log(distance)
+
+        if(distance<closer){
+            closer=distance
+            zoneNumber=myMap.get(locationName).zoneNumb
+        }    
+    }
+
+    return zoneNumber
+
 }
--- src/routes/special.routes.ts
+++ src/routes/special.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express'
 import passport from 'passport'
-import { findAll, addProduct } from '../controllers/prod.controllers'
+import { findAll, addProduct, getNearProducts } from '../controllers/prod.controllers'
 import Product from '../models/product'
 
 const router = Router()
 
 router.get('/listarProductos', passport.authenticate('jwt', { session: false }), findAll)
 
-router.get('/listarLatLong', passport.authenticate('jwt', { session: false }), )
+router.get('/listarLatLong', passport.authenticate('jwt', { session: false }), getNearProducts)
 
 router.post('/agregarProducto', passport.authenticate('jwt', { session: false }), addProduct)
 
